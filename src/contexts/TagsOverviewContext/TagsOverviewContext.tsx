102f0c0c92c6c4351f6d74e6c6862a7e244051bf
Bartłomiej Depciuch <depciuch322@gmail.com>
2024-03-29 00:13:27 +0100

    Added hooks and models to the context

--- src/contexts/TagsOverviewContext/TagsOverviewContext.tsx
+++ src/contexts/TagsOverviewContext/TagsOverviewContext.tsx
@@ -1,8 +1,9 @@
 import { ReactNode, createContext, useContext, useState } from 'react';
-import { ModelTagsOverviewContext } from './types/modelTagsOverviewContext';
+import { ModelTagsData, ModelTagsOverviewContext } from './types/modelTagsOverviewContext';
 
 const TagsOverviewContext = createContext<ModelTagsOverviewContext | undefined>(undefined);
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useTagsOverviewContext = () => {
   const context = useContext(TagsOverviewContext);
   if (context === undefined) {
@@ -13,12 +14,21 @@ export const useTagsOverviewContext = () => {
 
 export const TagsOverviewProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [tableRowsQuantity, setTableRowsQuantity] = useState<number>(20);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [tagsData, setTagsData] = useState<ModelTagsData[]>([]);
+  const [hasMoreData, setHasMoreData] = useState<boolean>(true);
 
   return (
     <TagsOverviewContext.Provider
       value={{
         tableRowsQuantity,
-        setTableRowsQuantity
+        setTableRowsQuantity,
+        currentPage,
+        setCurrentPage,
+        tagsData,
+        setTagsData,
+        hasMoreData,
+        setHasMoreData
       }}
     >
       {children}
--- src/contexts/TagsOverviewContext/types/modelTagsOverviewContext.ts
+++ src/contexts/TagsOverviewContext/types/modelTagsOverviewContext.ts
@@ -1,4 +1,18 @@
+export type ModelTagsData = {
+  has_synonyms: boolean;
+  is_moderator_only: false;
+  is_required: false;
+  count: number;
+  name: string;
+};
+
 export type ModelTagsOverviewContext = {
   tableRowsQuantity: number;
   setTableRowsQuantity: (quantity: number) => void;
+  tagsData: ModelTagsData[];
+  setTagsData: (tags: ModelTagsData[]) => void;
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
+  hasMoreData: boolean;
+  setHasMoreData: (hasMore: boolean) => void;
 };
