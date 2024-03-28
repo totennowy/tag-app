db5dae4b5e2e4fc70466c51bc80a5146b8e9d9e4
Bartłomiej Depciuch <depciuch322@gmail.com>
2024-03-29 00:12:31 +0100

    Added map logic into the table body

--- src/features/TagsOverview/components/TagsOverviewTableBody/TagsOverviewTableBody.tsx
+++ src/features/TagsOverview/components/TagsOverviewTableBody/TagsOverviewTableBody.tsx
@@ -1,12 +1,24 @@
-import  { FC } from 'react'
-import { TableBody } from '@mui/material'
+import { FC } from 'react';
+import { TableBody, TableCell, TableRow } from '@mui/material';
+import { useTagsOverviewContext } from '../../../../contexts/TagsOverviewContext/TagsOverviewContext';
+import { ModelTagsData } from '../../../../contexts/TagsOverviewContext/types/modelTagsOverviewContext';
+import useTagsOverview from '../../hooks/useTagsOverview';
 
-const TagsOverviewTableBody:FC = () => {
+const TagsOverviewTableBody: FC = () => {
+  const { tagsData } = useTagsOverviewContext();
+
+  useTagsOverview();
+
   return (
     <TableBody>
-       Table body
+      {tagsData?.map((tag: ModelTagsData, index: number) => (
+        <TableRow key={index}>
+          <TableCell>{tag.name}</TableCell>
+          <TableCell>{tag.count}</TableCell>
+        </TableRow>
+      ))}
     </TableBody>
-  )
-}
+  );
+};
 
-export default TagsOverviewTableBody
+export default TagsOverviewTableBody;
