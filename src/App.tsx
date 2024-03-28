cff478bb6273e6a20100b4b805a97378ec99a1b1
Bartłomiej Depciuch <depciuch322@gmail.com>
2024-03-29 00:12:50 +0100

    Added react-query logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { TagsOverviewProvider } from './contexts/TagsOverviewContext/TagsOverviewContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import TagsOverview from './features/TagsOverview/TagsOverview';
 import MainLayout from './layout/MainLayout';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <TagsOverviewProvider>
-      <MainLayout>
-        <TagsOverview />
-      </MainLayout>
-    </TagsOverviewProvider>
+    <QueryClientProvider client={queryClient}>
+      <TagsOverviewProvider>
+        <MainLayout>
+          <TagsOverview />
+        </MainLayout>
+      </TagsOverviewProvider>
+    </QueryClientProvider>
   );
 }
 
