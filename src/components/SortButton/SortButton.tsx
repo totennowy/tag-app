c8121a5b8269423d08447e033b2bf5794c219391
Bartłomiej Depciuch <depciuch322@gmail.com>
2024-04-03 22:42:35 +0200

    Added a filter to sort tags based on three categories

---
+++ src/components/SortButton/SortButton.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import { IconButton } from '@mui/material';
+import { ModelSortButton } from './types/modelSortButton';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+
+const SortButton: FC<ModelSortButton> = ({ sortType, onClick }) => {
+  return (
+    <IconButton onClick={onClick}>
+      {sortType === 'desc' ? (
+        <ArrowUpwardIcon fontSize="small" />
+      ) : (
+        <ArrowDownwardIcon fontSize="small" />
+      )}
+    </IconButton>
+  );
+};
+
+export default SortButton;
---
+++ src/components/SortButton/types/modelSortButton.ts
@@ -0,0 +1,4 @@
+export type ModelSortButton = {
+  onClick: () => void;
+  sortType: 'asc' | 'desc';
+};
